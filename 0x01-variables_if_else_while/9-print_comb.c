e0419b6886267ee85ebbf9737fcc0a321ce2bb86
MAhmoud Nawwar <asdasdfgh1234554321@gmail.com>
2023-03-19 11:33:46 -0700

    8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+
 /**
- * main -a program that prints all possible
- * combinations of single-digit numbers.
- *
- * Return: Always 0 (Success)
+ *main - print 0-9 separated with commas, using putchar
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	putchar('0');
-	for (int i = 1; i < 10; i++)
+	int n = '0';
+
+	while (n <= '9')
 	{
-	putchar(',');
-	putchar(' ');
-	putchar(i + '0');
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
 	}
 	putchar('\n');
+
 	return (0);
 }
