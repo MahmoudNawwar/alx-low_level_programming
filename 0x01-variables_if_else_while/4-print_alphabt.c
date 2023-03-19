2205908dcaf3240d4fbecee2a229c0dcc410e338
MAhmoud Nawwar <asdasdfgh1234554321@gmail.com>
2023-03-19 08:36:58 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -a program Write a program that prints the alphabet in lowercase,
+ *  followed by a new line.
+ *
+ * return (0) (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' || letter != 'e')
+		{
+			putchar(letter);
+		}
+		else
+			continue;
+		letter++;
+	}
+
+	puchar('\n');
+
+	return (0);
+}
