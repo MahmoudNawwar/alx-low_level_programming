d8ac8264cb30bfc35b71159db1bc75c088f1e731
MAhmoud Nawwar <asdasdfgh1234554321@gmail.com>
2023-03-23 08:30:40 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all numbers from 0x to 9x except 2x and 4x
+ * @void
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48' i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
+
